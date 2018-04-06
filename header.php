e32d0c8509f88f9699ea70474dffc7f9bcc4c90c
Larry Custodio <custodioly@gmail.com>
2018-04-06 00:08:09 -0700

    added the_loop feature for blog post display

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
     <div class="blog-masthead">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item active" href="<?php echo get_bloginfo('wpurl') ;?>">Home</a>
           <a class="blog-nav-item" href="#">New features</a>
           <a class="blog-nav-item" href="#">Press</a>
           <a class="blog-nav-item" href="#">New hires</a>
@@ -41,6 +41,10 @@
     <div class="container">
 
       <div class="blog-header">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        <h1 class="blog-title">
+          <a href="<?php echo get_bloginfo('wpurl') ;?>">
+            <?php echo get_bloginfo('name'); ?>
+          </a>
+        </h1>
+        <p class="lead blog-description"><?php echo get_bloginfo('description'); ?></p>
       </div>
