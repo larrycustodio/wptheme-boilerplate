7b62fdbc6d656e8a4b8755cda447766af059deb8
Larry Custodio <custodioly@gmail.com>
2018-04-06 08:42:29 -0700

    Added single-blog content templates

--- content.php
+++ content.php
@@ -1,8 +1,12 @@
 <div class="blog-post">
-    <h2 class="blog-post-title"><?php the_title(); ?></h2>
+    <h2 class="blog-post-title">
+        <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
+        </a>
+    </h2>
     <p class="blog-post-meta"><?php the_date(); ?>
         <a href="#"><?php the_author(); ?></a>
     </p>
-    <?php the_content(); ?>
+    <?php the_excerpt(); ?>
 </div>
 <!-- /.blog-post -->
--- page.php
+++ page.php
@@ -2,9 +2,9 @@
 
 	<div class="row">
 		<div class="col-sm-12">
-			<?php echo wp_title(''); ?>
-
-			<?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif; ?>
+			<?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+				the_content(); 
+			endwhile; endif; ?>
 		</div> <!-- /.col -->
 	</div> <!-- /.row -->
 
